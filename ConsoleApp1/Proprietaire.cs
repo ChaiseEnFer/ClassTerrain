b526f53c4208f2f693d6ff9d1b52bdb09c8c12ac
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-04 12:50:01 +0100

    classe Proprietaire

---
+++ ConsoleApp1/Proprietaire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Proprietaire
+    {
+        string Nom;
+        string Prenom;
+        List<string> Biens;
+
+        public string ToString()
+        {
+            string toString = String.Format($"Nom : {Nom}\n");
+            toString += String.Format($"Prénom : {Prenom}");
+            toString += String.Format($"liste des Biens {Biens}\n");
+
+            if (Biens == null) { toString += String.Format("Aucun Biens\n"); }
+            return toString;
+        }
+    }
+}
